4dd4df5cb2e333e10603ae81f82b8334ab7baa04
zhengmiaokai <965934999@qq.com>
2024-05-29 11:26:54 +0800

    [optimi] 优化代码结构

--- MKWebResources/URLScheme/MKURLSchemeHandler.m
+++ MKWebResources/URLScheme/MKURLSchemeHandler.m
@@ -57,6 +57,43 @@ static NSArray *MKWebFastURLSchemeFileTypes(void) {
     return self;
 }
 
+- (void)webView:(WKWebView *)webView startURLSchemeTask:(id <WKURLSchemeTask>)urlSchemeTask  API_AVAILABLE(ios(11.0)) {
+    // 适配iOS13的特性问题
+    [self _compatibilityIfNeed];
+    
+    NSMutableURLRequest *URLRequest = [urlSchemeTask.request mutableCopy];
+    NSURL *URL = urlSchemeTask.request.URL;
+    
+    [self.availableTasks addObject:urlSchemeTask];
+    
+    // 异步并行防止频繁的IO操作阻塞主线程
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        // 匹配本地资源文件
+        NSData *fileData = [[MKWebResourceLoader sharedInstance] obtainLocalResource:URL];
+        if (fileData) {
+            // WKURLSchemeHandler在MainQueue触发，请求响应也在MainQueue回调
+            dispatch_async(dispatch_get_main_queue(), ^{
+                NSLog(@"[webFast] startURLSchemeTask_localResource: %@", URL);
+                
+                [self _finishWithURLSchemeTask:urlSchemeTask fileData:fileData URL:URL];
+            });
+        } else {
+            NSLog(@"[webFast] startURLSchemeTask_remoteResource: %@", URL);
+            
+            [self _dataTaskWithRequest:URLRequest urlSchemeTask:urlSchemeTask];
+        }
+    });
+}
+
+- (void)webView:(WKWebView *)webView stopURLSchemeTask:(id <WKURLSchemeTask>)urlSchemeTask  API_AVAILABLE(ios(11.0)){
+    // stop如果不把urlSchemeTask标记为不可用，start时使用urlSchemeTask回写数据会导致crash 'This task has already been stopped'
+    if ([self.availableTasks containsObject:urlSchemeTask]) {
+        [self.availableTasks removeObject:urlSchemeTask];
+    }
+}
+
+#pragma mark - Getter -
+
 // 处理重定向、实时回调数据
 + (MKURLSessionManager *)sharedSessionManager {
     static dispatch_once_t once;
@@ -77,86 +114,74 @@ static NSArray *MKWebFastURLSchemeFileTypes(void) {
     return URLSession;
 }
 
-- (void)webView:(WKWebView *)webView startURLSchemeTask:(id <WKURLSchemeTask>)urlSchemeTask  API_AVAILABLE(ios(11.0)) {
-    // 适配iOS13的特性问题
-    [self _compatibilityIfNeed];
+#pragma mark - Private -
+
+- (void)_dataTaskWithRequest:(NSMutableURLRequest *)URLRequest urlSchemeTask:(id <WKURLSchemeTask>)urlSchemeTask API_AVAILABLE(ios(11.0)) {
+    [MKCookieSynchronizer syncRequestCookie:URLRequest];
+    [MKAjaxBodyAssembler syncRequestConfiguration:URLRequest];
     
-    NSMutableURLRequest *URLRequest = [urlSchemeTask.request mutableCopy];
-    NSURL *URL = urlSchemeTask.request.URL;
-    
-    [self.availableTasks addObject:urlSchemeTask];
-    
-    // 异步并行防止频繁的IO操作阻塞主线程
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        // 匹配本地资源文件
-        NSData *fileData = [[MKWebResourceLoader sharedInstance] obtainLocalResource:URL];
-        if (fileData) {
-            // WKURLSchemeHandler在MainQueue触发，请求响应也在MainQueue回调
-            dispatch_async(dispatch_get_main_queue(), ^{
-                if ([self.availableTasks containsObject:urlSchemeTask]) {
-                    NSLog(@"[webFast] startURLSchemeTask_localResource: %@", URL);
-                    
-                    NSMutableDictionary *headerFields = [NSMutableDictionary dictionaryWithDictionary:@{@"Access-Control-Allow-Origin": @"*"}];
-                    [headerFields wfSetObject:[MKWebFastURLSchemeContentTypes() wfStringForKey:URL.pathExtension] forKey:@"Content-Type"];
-                    
-                    NSHTTPURLResponse *response = [[NSHTTPURLResponse alloc] initWithURL:URL statusCode:200 HTTPVersion:(__bridge NSString *)kCFHTTPVersion1_1 headerFields:headerFields];
-                    [urlSchemeTask didReceiveResponse:response];
-                    [urlSchemeTask didReceiveData:fileData];
-                    [urlSchemeTask didFinish];
-                    
-                    [self.availableTasks removeObject:urlSchemeTask];
-                }
-            });
-        } else {
-            NSLog(@"[webFast] startURLSchemeTask_remoteResource: %@", URL);
-            
-            [MKCookieSynchronizer syncRequestCookie:URLRequest];
-            [MKAjaxBodyAssembler syncRequestConfiguration:URLRequest];
-            
-            // WKURLSchemeHandler在MainQueue触发，请求响应也在MainQueue回调
-            NSURLSessionTask *task = [[MKURLSchemeHandler sharedSessionManager] dataTaskWithRequest:URLRequest didReceiveResponse:^(NSURLSessionDataTask *dataTask, NSURLResponse *response) {
-                if ([self.availableTasks containsObject:urlSchemeTask]) {
-                    [urlSchemeTask didReceiveResponse:response];
-                }
-            } didReceiveData:^(NSURLSessionDataTask *dataTask, NSData *data) {
-                if ([self.availableTasks containsObject:urlSchemeTask]) {
-                    [urlSchemeTask didReceiveData:data];
-                }
-            } didComplete:^(NSURLSessionTask *task, NSError *error) {
-                if ([self.availableTasks containsObject:urlSchemeTask]) {
-                    if (error) {
-                        [urlSchemeTask didFailWithError:error];
-                    } else {
-                        [urlSchemeTask didFinish];
-                    }
-                    [self.availableTasks removeObject:urlSchemeTask];
-                }
-            } willPerformHTTPRedirection:^(NSHTTPURLResponse *response, NSURLRequest *request) {
-                if ([self.availableTasks containsObject:urlSchemeTask]) {
-                    NSData *selectorInfo = [[NSData alloc] initWithBase64EncodedString:@"X2RpZFBlcmZvcm1SZWRpcmVjdGlvbjpuZXdSZXF1ZXN0Og==" options:NSUTF8StringEncoding];
-                    NSString *selectorName = [[NSString alloc] initWithData:selectorInfo encoding:NSUTF8StringEncoding];
-                    SEL selector = NSSelectorFromString(selectorName);
-                    if ([urlSchemeTask respondsToSelector:selector]) {
+    // WKURLSchemeHandler在MainQueue触发，请求响应也在MainQueue回调
+    NSURLSessionTask *task = [[MKURLSchemeHandler sharedSessionManager] dataTaskWithRequest:URLRequest didReceiveResponse:^(NSURLSessionDataTask *dataTask, NSURLResponse *response) {
+        if ([self.availableTasks containsObject:urlSchemeTask]) {
+            [urlSchemeTask didReceiveResponse:response];
+        }
+    } didReceiveData:^(NSURLSessionDataTask *dataTask, NSData *data) {
+        if ([self.availableTasks containsObject:urlSchemeTask]) {
+            [urlSchemeTask didReceiveData:data];
+        }
+    } didComplete:^(NSURLSessionTask *task, NSError *error) {
+        if ([self.availableTasks containsObject:urlSchemeTask]) {
+            if (error) {
+                [urlSchemeTask didFailWithError:error];
+            } else {
+                [urlSchemeTask didFinish];
+            }
+            [self.availableTasks removeObject:urlSchemeTask];
+        }
+    } willPerformHTTPRedirection:^(NSHTTPURLResponse *response, NSURLRequest *request) {
+        if ([self.availableTasks containsObject:urlSchemeTask]) {
+            NSData *selectorInfo = [[NSData alloc] initWithBase64EncodedString:@"X2RpZFBlcmZvcm1SZWRpcmVjdGlvbjpuZXdSZXF1ZXN0Og==" options:NSUTF8StringEncoding];
+            NSString *selectorName = [[NSString alloc] initWithData:selectorInfo encoding:NSUTF8StringEncoding];
+            SEL selector = NSSelectorFromString(selectorName);
+            if ([urlSchemeTask respondsToSelector:selector]) {
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
-                        [urlSchemeTask performSelector:selector withObject:response withObject:request];
+                [urlSchemeTask performSelector:selector withObject:response withObject:request];
 #pragma clang diagnostic pop
-                    }
-                }
-            }];
-            [task resume];
+            }
         }
-    });
+    }];
+    [task resume];
 }
 
-- (void)webView:(WKWebView *)webView stopURLSchemeTask:(id <WKURLSchemeTask>)urlSchemeTask  API_AVAILABLE(ios(11.0)){
-    // stop如果不把urlSchemeTask标记为不可用，start时使用urlSchemeTask回写数据会导致crash 'This task has already been stopped'
+- (void)_finishWithURLSchemeTask:(id <WKURLSchemeTask>)urlSchemeTask fileData:(NSData *)fileData URL:(NSURL *)URL API_AVAILABLE(ios(11.0)) {
     if ([self.availableTasks containsObject:urlSchemeTask]) {
+        NSMutableDictionary *headerFields = [NSMutableDictionary dictionaryWithDictionary:@{@"Access-Control-Allow-Origin": @"*"}];
+        [headerFields wfSetObject:[MKWebFastURLSchemeContentTypes() wfStringForKey:URL.pathExtension] forKey:@"Content-Type"];
+        
+        NSHTTPURLResponse *response = [[NSHTTPURLResponse alloc] initWithURL:URL statusCode:200 HTTPVersion:(__bridge NSString *)kCFHTTPVersion1_1 headerFields:headerFields];
+        [urlSchemeTask didReceiveResponse:response];
+        [urlSchemeTask didReceiveData:fileData];
+        [urlSchemeTask didFinish];
+        
         [self.availableTasks removeObject:urlSchemeTask];
     }
 }
 
-#pragma mark - Private -
+- (void)_failWithURLSchemeTask:(id <WKURLSchemeTask>)urlSchemeTask fileData:(NSData *)fileData error:(NSError *)error URL:(NSURL *)URL API_AVAILABLE(ios(11.0)) {
+    if ([self.availableTasks containsObject:urlSchemeTask]) {
+        NSMutableDictionary *headerFields = [NSMutableDictionary dictionaryWithDictionary:@{@"Access-Control-Allow-Origin": @"*"}];
+        [headerFields wfSetObject:[MKWebFastURLSchemeContentTypes() wfStringForKey:URL.pathExtension] forKey:@"Content-Type"];
+        
+        NSHTTPURLResponse *response = [[NSHTTPURLResponse alloc] initWithURL:URL statusCode:200 HTTPVersion:(__bridge NSString *)kCFHTTPVersion1_1 headerFields:headerFields];
+        [urlSchemeTask didReceiveResponse:response];
+        [urlSchemeTask didReceiveData:fileData];
+        [urlSchemeTask didFailWithError:error];
+        
+        [self.availableTasks removeObject:urlSchemeTask];
+    }
+}
+
 - (void)_compatibilityIfNeed {
     // 兼容iOS13 xhr upload的crash
     NSString *systemVersion = [UIDevice currentDevice].systemVersion;
